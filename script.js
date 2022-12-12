b8e26e1c366dab5e5903e7270bf0a4b7768dd353
Vinh Quyen <vinhquyenton.1995@gmail.com>
2022-12-11 21:24:50 -0800

    Fix bug in createBoard function

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
 function createBoard(size){
   let board = document.querySelector(".board");
 
-  board.style.gridTemplateColumns = `repeat (${size} , 1fr)`;
-  board.style.gridTemplateRows = `repeat (${size} , 1fr)`;
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
   let numDivs = size * size;
 
   for(i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
+    div.style.backgroundColor = "yellow";
     board.insertAdjacentElement("beforeend",div);
   }
+  
 }
